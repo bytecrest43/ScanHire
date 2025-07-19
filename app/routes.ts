fb9544661106285a5e153748a715f3fed6aa298d
ByteCrest <lbwiththecodes@gmail.com>
2025-07-19 16:06:21 -0700

    Auth & Puter Integration

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,13 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    route('/auth', 'routes/auth.tsx'),
+] satisfies RouteConfig;
+function route(path: string, file: string): import("@react-router/dev/routes").RouteConfigEntry {
+    return {
+        path,
+        file,
+    };
+}
+
