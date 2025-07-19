49a6dd670869b72422a193f14cd8961d3c2eb0a2
ByteCrest <lbwiththecodes@gmail.com>
2025-07-19 14:05:34 -0700

    implementing Homepage, Navbar & ResumeCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "constants/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ScanHire" },
+    {
+      name: "description",
+      content:
+        "Scanhire is an AI-powered resume analyzer that helps" +
+        " job seekers optimize their resumes for Applicant Tracking Systems (ATS)!",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-18">
+          <h1>Track Your Applications & Resume Rating</h1>
+          <h2>
+            Upload your resumes and check AI-powered reviews and feedbacks
+          </h2>
+        </div>
+      {resumes.length > 0 &&  (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+      </section>
+    </main>
+  );
 }
