134140483c6cd93964e9f126afabb0b3ee148411
ByteCrest <lbwiththecodes@gmail.com>
2025-07-20 08:36:49 -0700

    Add Upload Form and FileUploader Component

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import { type RouteConfig, index } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
+    route('/upload', 'routes/upload.tsx')
 ] satisfies RouteConfig;
 function route(path: string, file: string): import("@react-router/dev/routes").RouteConfigEntry {
     return {
