e225918b346812b0afb67ecf1504d99895c164e1
ByteCrest <lbwiththecodes@gmail.com>
2025-07-23 15:24:01 -0700

    Implement funtionality to fetch Real Data

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,30 +1,27 @@
-import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router";
+
+import {Link, useNavigate, useParams} from "react-router";
+import {useEffect, useState} from "react";
+import {usePuterStore} from "~/lib/puter";
+import Summary from "~/components/Summary";
 import ATS from "~/components/ATS";
 import Details from "~/components/Details";
-import Summary from "~/components/Summary";
-import { usePuterStore } from "~/lib/puter";
 
-export const meta = () => [
-  { title: "ScanHire | Review" },
-  { name: "description", content: "Detail Analysis of your Resume/CV" },
-];
+export const meta = () => ([
+    { title: 'ScanHire | Review ' },
+    { name: 'description', content: 'Detailed overview of your resume' },
+])
 
-const resume = () => {
-    const { auth, isLoading, fs, kv} = usePuterStore();
+const Resume = () => {
+    const { auth, isLoading, fs, kv } = usePuterStore();
     const { id } = useParams();
     const [imageUrl, setImageUrl] = useState('');
     const [resumeUrl, setResumeUrl] = useState('');
     const [feedback, setFeedback] = useState<Feedback | null>(null);
     const navigate = useNavigate();
 
-
-
-
- useEffect(() => {
+    useEffect(() => {
         if(!isLoading && !auth.isAuthenticated) navigate(`/auth?next=/resume/${id}`);
-    }, [isLoading]);
-
+    }, [isLoading])
 
     useEffect(() => {
         const loadResume = async () => {
@@ -52,20 +49,17 @@ const resume = () => {
 
         loadResume();
     }, [id]);
-  return (
-    <main className="!pt-0">
-        <nav className="resume-nav">
-            <Link to="/" className="back-button">
-              <img
-                src="/icons/back.svg"
-                alt="logo"
-                className="w-2.5 h-2.5"
-              />
-              <span className="text-gray-800 text-sm font-semibold">Back</span>
-            </Link>
-        </nav>
-        <div className="flex flex-row w-full max-lg:flex-col-reverse">
-            <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
+
+    return (
+        <main className="!pt-0">
+            <nav className="resume-nav">
+                <Link to="/" className="back-button">
+                    <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
+                    <span className="text-gray-800 text-sm font-semibold">Back</span>
+                </Link>
+            </nav>
+            <div className="flex flex-row w-full max-lg:flex-col-reverse">
+                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
                     {imageUrl && resumeUrl && (
                         <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
                             <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
@@ -77,29 +71,21 @@ const resume = () => {
                             </a>
                         </div>
                     )}
-            </section>
-            <section className="feedback-section">
-                <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
-
-                {feedback ? (
-                    <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                        <Summary feedback={feedback}/>
-                        <ATS  score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
-                        <Details feedback={feedback} />
-                    </div>
-                ) : (
-                    <img 
-                      src="/images/resume-scan-2.gif"
-                        alt="loading"
-                        className="w-full h-full "
-                    />
-                )}
-            </section>
-              
-
-        </div>
-    </main>
-  )
+                </section>
+                <section className="feedback-section">
+                    <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
+                    {feedback ? (
+                        <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
+                            <Summary feedback={feedback} />
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
+                            <Details feedback={feedback} />
+                        </div>
+                    ) : (
+                        <img src="/images/resume-scan-2.gif" alt="Loading..." className="w-full" />
+                    )}
+                </section>
+            </div>
+        </main>
+    )
 }
-
-export default resume
+export default Resume
